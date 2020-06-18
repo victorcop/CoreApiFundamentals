92cb09f921b7795b6eb58d073f5e8a77a17f67f5
Victor Velasquez <victorcop90@gmail.com>
2020-06-18 06:28:37 -0500

    CampsController

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCamps()
+        {
+            return Ok(new { Moniker = "ATL2018", Name = "Atlanta Code Camp" });
+        }
+    }
+}
