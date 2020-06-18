945a8bcfd5707c100073a788c58634ddc6c5ff0d
Victor Velasquez <victorcop90@gmail.com>
2020-06-18 15:13:54 -0500

    Talks-Gets

---
+++ src/Controllers/TalksController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/camps/{moniker}/[controller]")]
+    [ApiController]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public TalksController(ICampRepository campRepository, IMapper mapper,
+            LinkGenerator linkGenerator)
+        {
+            _campRepository = campRepository;
+            _mapper = mapper;
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TalksModel>>> Get(string moniker)
+        {
+            try
+            {
+                var takls = await _campRepository.GetTalksByMonikerAsync(moniker);
+
+                if (takls == null) return NotFound();
+                return Ok(_mapper.Map<IEnumerable<TalksModel>>(takls));
+            }
+            catch (System.Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+            
+        }
+    }
+}
